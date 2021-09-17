985a98c9e1a9576f4fb871851e6cad3dc2844588
gabi <gabriella.marias@gmail.com>
2021-09-16 21:30:15 -0400

    Second commit for midterm project

---
+++ Sid-Stephanie-Gabi Midterm OOP/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sid_Stephanie_Gabi_Midterm_OOP
+{
+    abstract class Materials
+    {
+        public static string nameOfMaterial { get; set; }
+
+        public static string Creator { get; set; }
+
+        public enum Status { }
+
+        public static string dueDate { get; set; }
+
+        
+
+    }
+}
