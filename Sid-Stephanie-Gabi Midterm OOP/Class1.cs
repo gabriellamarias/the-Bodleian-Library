5a9aa76606bbaf0caaf7207ae7375e1135264266
gabi <gabriella.marias@gmail.com>
2021-09-22 22:11:59 -0400

    Wednesday updates

--- Sid-Stephanie-Gabi Midterm OOP/Class1.cs
+++ Sid-Stephanie-Gabi Midterm OOP/Class1.cs
@@ -1,20 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
+using System.IO;
+
 
 namespace Sid_Stephanie_Gabi_Midterm_OOP
 {
-    abstract class Materials
+
+    public enum Status
     {
-        public static string nameOfMaterial { get; set; }
+        checkedOut,
+        onShelf,
+        vanished
 
-        public static string Creator { get; set; }
+    }
+    public abstract class Materials
+    {
+        public abstract string typeOfMaterial { get; set; }
+        public abstract string nameOfMaterial { get; set; }
 
-        public enum Status { }
+        public abstract string Creator { get; set; }
 
-        public static string dueDate { get; set; }
+        public abstract Status statusOfMaterial { get; set; }
 
-        
+        public abstract string dueDate { get; set; }
+
+    }
+    public class potionsSupplies : Materials
+    {
+        private string _typeofmaterial = "Potions Supplies";
+        public override string typeOfMaterial { get { return _typeofmaterial; } set { _typeofmaterial = value; } }
+
+        public override string nameOfMaterial { get; set; }
+        public override string Creator { get; set; }
+
+        public override Status statusOfMaterial { get; set; }
+
+        public override string dueDate { get; set; }
+
+        public potionsSupplies(string nameOfMaterial, string Creator, Status itemStatus)
+        {
+
+            this.nameOfMaterial = nameOfMaterial;
+            this.Creator = Creator;
+            this.statusOfMaterial = itemStatus;
+        }
+
+    }
+    public class Book : Materials
+    {
+        private string _typeofmaterial = "Books";
+        public override string typeOfMaterial { get { return _typeofmaterial; } set { _typeofmaterial = value; } }
+
+        public override string nameOfMaterial { get; set; }
+        public override string Creator { get; set; }
+
+        public override Status statusOfMaterial { get; set; }
+
+        public override string dueDate { get; set; }
+
+        public Book(string Title, string Author, Status itemStatus)
+        {
+
+            this.nameOfMaterial = Title;
+            this.Creator = Author;
+            this.statusOfMaterial = itemStatus;
+        }
+    }
+    public class Manga : Materials
+    {
+        private string _typeofmaterial = "Manga";
+        public override string typeOfMaterial { get { return _typeofmaterial; } set { _typeofmaterial = value; } }
+
+        public override string nameOfMaterial { get; set; }
+        public override string Creator { get; set; }
+
+        public override Status statusOfMaterial { get; set; }
+
+        public override string dueDate { get; set; }
+
+        public Manga(string Title, string Author, Status itemStatus)
+        {
+
+            this.nameOfMaterial = Title;
+            this.Creator = Author;
+            this.statusOfMaterial = itemStatus;
+        }
 
     }
 }
--- Sid-Stephanie-Gabi Midterm OOP/Program.cs
+++ Sid-Stephanie-Gabi Midterm OOP/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.IO;
+using Sid_Stephanie_Gabi_Midterm_OOP;
 
 namespace the_Bodleian_Library
 {
@@ -6,7 +11,49 @@ namespace the_Bodleian_Library
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            List<Materials> allMaterials = new List<Materials>();
+
+            allMaterials.Add(new potionsSupplies("Pewter Cauldron", "Ancient Potion Maker", Status.onShelf));
+            allMaterials.Add(new potionsSupplies("Glass Phial", "Diagon Alley Apothecary", Status.onShelf));
+            allMaterials.Add(new potionsSupplies("Crystal Phial", "Potions Lady", Status.onShelf));
+            allMaterials.Add(new potionsSupplies("Brass Scales", "Ancient Potion Maker", Status.onShelf));
+            allMaterials.Add(new potionsSupplies("Juicing Board", "Diagon Alley Apothecary", Status.onShelf));
+            allMaterials.Add(new potionsSupplies("Mortar and Pestle", "Ancient Potion Maker", Status.onShelf));
+            allMaterials.Add(new Book("Fantastic Beasts and Where to Find Them", "Newt Scamander", Status.onShelf));
+            allMaterials.Add(new Book("Quidditch Through the Ages", "Kennilworthy Whisp", Status.onShelf));
+            allMaterials.Add(new Book("Hogwarts: A History", "Bathilda Bagshot", Status.onShelf));
+            allMaterials.Add(new Book("Magical Drafts and Potions", "Arsenic Jigger", Status.onShelf));
+            allMaterials.Add(new Book("Unfogging the Future", "Blenheim Stalk", Status.onShelf));
+            allMaterials.Add(new Book("Magical Me", "Gilderoy Lockhart", Status.onShelf));
+            allMaterials.Add(new Manga("Azuallmanga Daioh", "Kiyohiko Azuma", Status.onShelf));
+            allMaterials.Add(new Manga("K-On", "Kakifly", Status.onShelf));
+            allMaterials.Add(new Manga("Lucky Star", "Kagami Yoshimizu", Status.onShelf));
+            allMaterials.Add(new Manga("Daily lives of high school boys", "Yasunobu Yamauchi", Status.onShelf));
+            allMaterials.Add(new Manga("Nichijou", "Keiichi Arawi", Status.onShelf));
+            allMaterials.Add(new Manga("Kaguya-sama: Love is War", "Aka Akasaka", Status.onShelf));
+
+            string Path = "MaterialsList.txt";
+
+
+            using (TextWriter tw = new StreamWriter(Path))
+            {
+                foreach (Materials x in allMaterials)
+                {
+                    tw.WriteLine(string.Format("Material: {0} | Name: {1} | Creator: {2} | Status: {3}", x.typeOfMaterial, x.nameOfMaterial, x.Creator, x.statusOfMaterial.ToString()));
+                }
+            }
+
+            using (StreamReader reader = new StreamReader(Path))
+            {
+                var line = string.Empty;
+                while (!reader.EndOfStream)
+                {
+                    line = reader.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
         }
     }
 }
+              
